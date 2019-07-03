094ced43051af192f3019ffcb4c93294691308f1
usman <usman@SEM-LT-CLIENT>
2019-07-03 18:56:49 +0500

    TEst Again

--- ShanuNestedDataGridView/Helper/ShanuDGVHelper.cs
+++ ShanuNestedDataGridView/Helper/ShanuDGVHelper.cs
@@ -20,6 +20,7 @@ namespace ShanuNestedDataGridView.Helper
 {
     class ShanuDGVHelper
     {
+        // test by usman 
         #region Variables
         public DataGridView MasterDGVs = new DataGridView();
         public DataGridView DetailDGVs = new DataGridView();
